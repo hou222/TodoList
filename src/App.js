f22268cc734b9b7f83f2b8d9ffa38a5e95499e24
Houssam Benlagha <houssambenlagha0@gmail.com>
2023-12-01 11:30:13 +0100

    last update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [toDoList, setToDoList] = useState([]);
+  const [editItem, setEditItem] = useState("");
+
+  function hundleAddToDo(todo) {
+    setToDoList((toDoList) => [...toDoList, todo]);
+  }
+  function hundleDeleteToDo(todo) {
+    setToDoList((toDoList) => toDoList.filter((toDo) => toDo !== todo));
+  }
+
+  function hundleEditToDo(todo) {
+    setToDoList((toDoList) =>
+      toDoList.map((toDo) => (toDo === editItem ? todo : toDo))
+    );
+    setEditItem("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Get Things Done!</h1>
+      <Input onSetToDo={hundleAddToDo} />
+      {toDoList.map((todo, i) =>
+        editItem !== todo ? (
+          <Item
+            todo={todo}
+            onDelete={hundleDeleteToDo}
+            onEdit={setEditItem}
+            key={i}
+          />
+        ) : (
+          <Input editItem={editItem} onChange={hundleEditToDo} key={i} />
+        )
+      )}
     </div>
   );
 }
 
-export default App;
+function Input({ onSetToDo, editItem, onChange }) {
+  const [toDo, setToDo] = useState("");
+  return (
+    <div>
+      <div>
+        {editItem ? (
+          <input
+            type="text"
+            value={editItem}
+            onChange={(e) => onChange(e.target.value)}
+          />
+        ) : (
+          <input
+            type="text"
+            placeholder="What is the task today ?"
+            value={toDo}
+            onChange={(e) => setToDo(e.target.value)}
+          />
+        )}
+
+        <button onClick={() => onSetToDo(toDo)}>Add Task</button>
+      </div>
+    </div>
+  );
+}
+
+function Item({ todo, onDelete, onEdit }) {
+  //const [deleteToDo, setDeleteToDo] = useState("");
+  return (
+    <div className="item">
+      <span>{todo}</span>
+      <div className="settings">
+        <img src="edit.png" alt="edit" onClick={() => onEdit(todo)} />
+        <img src="trash.png" alt="trash" onClick={() => onDelete(todo)} />
+      </div>
+    </div>
+  );
+}
